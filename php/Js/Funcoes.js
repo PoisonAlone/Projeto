f40af4938fdb4b490a00488d9c1e4af6f6b5a6ac
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-01-04 18:32:59 -0300

    php e javaScript válidando (Luiz Carlos)

--- php/Js/Funcoes.js
+++ php/Js/Funcoes.js
@@ -1,23 +1,29 @@
+
 function validar() {
+    event.preventDefault();
     if((document.getElementById("login").value == "") || (document.getElementById("login").value == null)){
         var hiden = document.getElementById("hidden");
         document.getElementById("h1").innerHTML = 'Valores Inválidos ou NULL';
+        resultado = "inválido";
         hiden.value = "inválido";
 
     }
     else if((document.getElementById("senha").value == "") || (document.getElementById("senha").value == null)){
         var hiden = document.getElementById("hidden");
         document.getElementById("h1").innerHTML = 'Valores Inválidos ou NULL';
+        resultado = "inválido";
         hiden.value = "inválido";
     }
     else if((document.getElementById("nome").value == "") || (document.getElementById("nome").value == null)){
         var hiden = document.getElementById("hidden");
         document.getElementById("h1").innerHTML = 'Valores Inválidos ou NULL';
+        resultado = "inválido";
         hiden.value = "inválido";
     }
     else{
         var hiden = document.getElementById("hidden");
         document.getElementById("h1").innerHTML = "Valor Válidos";
+        resultado = "válido";
         hiden.value = "válido";
     }
 }
