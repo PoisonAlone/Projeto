82dbf13198490195ee2d15ab0e6d28f5a5f39b9e
scsSilva <sallys.carlos93@gmail.com>
2020-01-04 19:33:15 -0300

    Modificando o arquivo js de Diretório

---
+++ js/Funcoes.js
@@ -0,0 +1,43 @@
+
+function validarCadastroUsu() {
+
+    event.preventDefault();
+    if((document.getElementById("login").value == "") || (document.getElementById("login").value == null)){
+        var hiden = document.getElementById("hidden");
+
+        var h1 = document.getElementById("h1");
+        h1.innerHTML = 'Valores Inválidos ou NULL';
+        h1.style.color = 'red';
+        
+        hiden.value = "inválido";
+
+    }
+    else if((document.getElementById("senha").value == "") || (document.getElementById("senha").value == null)){
+        var hiden = document.getElementById("hidden");
+        
+        var h1 = document.getElementById("h1");
+        h1.innerHTML = 'Valores Inválidos ou NULL';
+        h1.style.color = 'red';
+
+        hiden.value = "inválido";
+    }
+    else if((document.getElementById("nome").value == "") || (document.getElementById("nome").value == null)){
+        var hiden = document.getElementById("hidden");
+        
+        var h1 = document.getElementById("h1");
+        h1.innerHTML = 'Valores Inválidos ou NULL';
+        h1.style.color = 'red';
+        
+        hiden.value = "inválido";
+    }
+    else{
+        var hiden = document.getElementById("hidden");
+        
+        var h1 = document.getElementById("h1");
+        h1.innerHTML = 'Valores Válidos';
+        h1.style.color = 'green';
+        
+        hiden.value = "válido";
+
+    }
+}
