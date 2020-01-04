819fc10188138d6b2bb6c2477449ab923f2bff61
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-01-04 18:59:56 -0300

    PHP e JavaScript validando (Luiz Carlos)

--- php/Js/Funcoes.js
+++ php/Js/Funcoes.js
@@ -1,29 +1,43 @@
 
 function validar() {
+    
     event.preventDefault();
     if((document.getElementById("login").value == "") || (document.getElementById("login").value == null)){
         var hiden = document.getElementById("hidden");
-        document.getElementById("h1").innerHTML = 'Valores Inválidos ou NULL';
-        resultado = "inválido";
+
+        var h1 = document.getElementById("h1");
+        h1.innerHTML = 'Valores Inválidos ou NULL';
+        h1.style.color = 'red';
+        
         hiden.value = "inválido";
 
     }
     else if((document.getElementById("senha").value == "") || (document.getElementById("senha").value == null)){
         var hiden = document.getElementById("hidden");
-        document.getElementById("h1").innerHTML = 'Valores Inválidos ou NULL';
-        resultado = "inválido";
+        
+        var h1 = document.getElementById("h1");
+        h1.innerHTML = 'Valores Inválidos ou NULL';
+        h1.style.color = 'red';
+
         hiden.value = "inválido";
     }
     else if((document.getElementById("nome").value == "") || (document.getElementById("nome").value == null)){
         var hiden = document.getElementById("hidden");
-        document.getElementById("h1").innerHTML = 'Valores Inválidos ou NULL';
-        resultado = "inválido";
+        
+        var h1 = document.getElementById("h1");
+        h1.innerHTML = 'Valores Inválidos ou NULL';
+        h1.style.color = 'red';
+        
         hiden.value = "inválido";
     }
     else{
         var hiden = document.getElementById("hidden");
-        document.getElementById("h1").innerHTML = "Valor Válidos";
-        resultado = "válido";
+        
+        var h1 = document.getElementById("h1");
+        h1.innerHTML = 'Valores Válidos';
+        h1.style.color = 'green';
+        
         hiden.value = "válido";
+
     }
 }
